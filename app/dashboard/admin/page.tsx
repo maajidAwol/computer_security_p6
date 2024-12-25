6bae414d4123cfa2256bce091aad8bbd36c21712
Abdulmajid Awol <maajidawol@gmail.com>
2024-12-26 01:01:22 +0300

    color

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -373,10 +373,10 @@ export default function AdminDashboard() {
   if (!user) return null
 
   return (
-    <div className="min-h-screen bg-gray-100 text-white">
+    <div className="min-h-screen bg-gray-100 ">
       <header className="bg-white bg-opacity-10 backdrop-blur-lg shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-          <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+          <h1 className="text-3xl  font-bold">Admin Dashboard</h1>
           <Button variant="destructive" onClick={handleLogout}>
             Logout
           </Button>
